e39e4fe1200e7de4cbf7a7cbb36aa0da6b86146e
linSir6 <18304523113@163.com>
2017-04-20 18:14:34 +0800

    add raiders

--- app/src/main/java/com/wenxin/dongyouji/fragment/RaidersFragment.java
+++ app/src/main/java/com/wenxin/dongyouji/fragment/RaidersFragment.java
@@ -9,10 +9,19 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.squareup.okhttp.Request;
 import com.wenxin.dongyouji.R;
+import com.wenxin.dongyouji.Url;
 import com.wenxin.dongyouji.adapter.RaidersAdapter;
 import com.wenxin.dongyouji.model.RaidersModel;
+import com.wenxin.dongyouji.model.TravelNotesModel;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +42,53 @@ public class RaidersFragment extends Fragment {
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
         adapter = new RaidersAdapter();
         List<RaidersModel> lists = new ArrayList<RaidersModel>();
+
+
+        String url = Url.url + "get_all_travel_notes";
+        OkHttpUtils
+                .get()
+                .url(url)
+                .addParams("null", "null")
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Request request, Exception e) {
+                        Toast.makeText(getActivity(), "获取游记失败", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResponse(String response) {
+
+                        List<TravelNotesModel> list = new ArrayList<TravelNotesModel>();
+
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+                            for (int i = 0; i < jsonArray.length(); i++) {
+
+                                TravelNotesModel travelNotesModel = new TravelNotesModel(
+                                        jsonArray.getJSONObject(i).optString("title"),
+                                        jsonArray.getJSONObject(i).optString("date"),
+                                        jsonArray.getJSONObject(i).optString("background"),
+                                        jsonArray.getJSONObject(i).optString("text1"),
+                                        jsonArray.getJSONObject(i).optString("img1"),
+                                        jsonArray.getJSONObject(i).optString("text2")
+                                );
+
+                                list.add(travelNotesModel);
+
+                            }
+
+                            adapter.refreshList(list);
+                            mList = list;
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                });
+
+
         RaidersModel a1 = new RaidersModel();
         a1.setTitle("日本\nJAPAN");
         a1.setCount("1010旅行地");
